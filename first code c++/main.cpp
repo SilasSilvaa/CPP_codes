efeb05590fc90346db506a52df7acd8d3daba39f
silassilvaa <silas-oli@hotmail.com>
2025-01-07 09:49:03 -0300

    first code with c++

---
+++ first code c++/main.cpp
@@ -0,0 +1,11 @@
+//Load libraies
+#include <iostream> 
+
+int main(){
+    int a = 2;
+    int b = 2
+
+    std::cout << a + b << std::endl;
+    std::cout << "Hello world" << std::endl;
+    std::cout << "Hello world";
+}
